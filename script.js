c8872f1045a9b05ee56210ca64f31a65869e149c
RicardoSilvaF <zanphs@gmail.com>
2022-10-23 18:32:32 -0300

    Final do html/css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+
+
+
 let quant = Number(prompt("Digite o número de cartas que deseja(apenas números pares de 4 a 14):"));
 while(quant % 2 !== 0 || quant<4 || quant>14){
     alert("Número inválido!");
     quant = Number(prompt("Digite o número de cartas que deseja(apenas números pares de 4 a 14):"));
 }
+
